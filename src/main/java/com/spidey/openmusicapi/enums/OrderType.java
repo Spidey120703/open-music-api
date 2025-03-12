9a6be365273d43ed659b7ebeadf4dfa659775b3f
Peter Pan <94622843+Spidey120703@users.noreply.github.com>
2025-03-12 15:01:24 +0800

    扩充枚举功能 OrderType

--- src/main/java/com/spidey/openmusicapi/enums/OrderType.java
+++ src/main/java/com/spidey/openmusicapi/enums/OrderType.java
@@ -7,14 +7,22 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum OrderType {
+public class OrderType {
 
-    ASC("asc", "升序"),
-    DESC("desc", "降序");
+    public final static OrderType ASC = new OrderType("asc", "升序");
+    public final static OrderType DESC = new OrderType("desc", "降序");
 
     @EnumValue
     @JsonValue
     private final String type;
     private final String desc;
+
+    public static OrderType valueOf(String type) {
+        if (DESC.getType().equalsIgnoreCase(type)) {
+            return DESC;
+        } else {
+            return ASC;
+        }
+    }
 
 }
